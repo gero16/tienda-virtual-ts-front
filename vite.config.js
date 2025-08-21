6aaa3499fa311e6e78b1b0db24f597d85643ef82
gero <geronicola1696@gmail.com>
2025-08-20 22:21:33 -0300

    arreglos urls

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default {
         input: {
           main: path.resolve(__dirname, './index.html'), // Tu página principal
           tienda: path.resolve(__dirname, 'src/html/tienda.html'), // Tu página de tienda
-          checkout: path.resolve(__dirname, 'src/html/checkout.html')
+          checkout: path.resolve(__dirname, 'src/html/checkout.html'),
+          contacto: path.resolve(__dirname, 'src/html/contacto.html'),
+          tiendaML: path.resolve(__dirname, 'src/html/tiendaML.html'),
+          detalleProducto: path.resolve(__dirname, 'src/html/detalleProducto.html')
         },
       },
     },
